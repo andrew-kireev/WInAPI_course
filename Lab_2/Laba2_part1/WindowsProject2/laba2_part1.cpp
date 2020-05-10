353fc779b262f197bca4df47babb74898d0db95f
Андрей Киреев <a-kireev1989@mail.ru>
2020-05-10 12:46:31 +0300

    add laba_2

---
+++ Lab_2/Laba2_part1/WindowsProject2/laba2_part1.cpp
@@ -0,0 +1,108 @@
+﻿
+
+// Global variables
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <iostream>
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+WCHAR WinName[] = _TEXT("MainFrame");
+WCHAR WinName2[] = _TEXT("MainFrame2");
+
+
+int WINAPI wWinMain(HINSTANCE This,		 // Дескриптор текущего приложения 
+	HINSTANCE Prev, 	// В современных системах всегда 0 
+	LPTSTR cmd, 		// Командная строка 
+	int mode) 		// Режим отображения окна
+{
+	HWND hWnd;		// Дескриптор главного окна программы 
+	MSG msg; 		// Структура для хранения сообщения 
+	WNDCLASS wc; 	// Класс окна
+	// Определение класса окна 
+	wc.hInstance = This;
+	wc.lpszClassName = WinName; 				// Имя класса окна 
+	wc.lpfnWndProc = WndProc; 					// Функция окна 
+	wc.style = CS_HREDRAW | CS_VREDRAW; 			// Стиль окна 
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); 		// Стандартная иконка 
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 		// Стандартный курсор 
+	wc.lpszMenuName = NULL; 					// Нет меню 
+	wc.cbClsExtra = 0; 						// Нет дополнительных данных класса 
+	wc.cbWndExtra = 0; 						// Нет дополнительных данных окна 
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 	// Заполнение окна белым цветом 
+
+
+
+	// Регистрация класса окна
+	if (!RegisterClass(&wc)) return 0;
+
+	// Создание окна 
+	hWnd = CreateWindow(WinName,			// Имя класса окна 
+		_TEXT("Окно № 1"), 		// Заголовок окна 
+		WS_OVERLAPPEDWINDOW, 		// Стиль окна 
+		CW_USEDEFAULT,				// x 
+		CW_USEDEFAULT, 				// y	 Размеры окна 
+		600, 				// width 
+		500, 				// Height 
+		HWND_DESKTOP, 				// Дескриптор родительского окна 
+		NULL, 						// Нет меню 
+		This, 						// Дескриптор приложения 
+		NULL); 					// Дополнительной информации нет 
+
+
+	//SetWindowLong(, GWL_STYLE, WS_CHILD);
+	ShowWindow(hWnd, mode); 				// Показать окно
+
+	// Цикл обработки сообщений 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); 		// Функция трансляции кодов нажатой клавиши 
+		DispatchMessage(&msg); 		// Посылает сообщение функции WndProc() 
+	}
+	return 0;
+}
+
+// Оконная функция вызывается операционной системой
+// и получает сообщения из очереди для данного приложения
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	HWND descriptor;
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+	default: 			// Обработка сообщения по умолчанию 
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	case WM_LBUTTONDOWN:
+	{
+		descriptor = FindWindow(WinName2, NULL);
+		if (descriptor != NULL)
+		{
+			MessageBox(hWnd, _TEXT("Окно 2 нашли посылаем сообщение"), _TEXT("Нашли"), MB_OK);
+			SendMessage(descriptor, WM_USER + 1, WPARAM(hWnd), NULL);
+		}
+		else
+		{
+			MessageBox(hWnd, _TEXT("Окно № 2 закрыто"), _TEXT("Окно № 2 не найдено"), MB_OK);
+		}
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	case WM_RBUTTONDOWN:
+	{
+		descriptor = FindWindow(WinName2, NULL);
+		if (descriptor != NULL)
+		{
+			MessageBox(hWnd, _TEXT("Окно 2 нашли посылаем сообщение выключения!"), _TEXT("Нашли"), MB_OK);
+			SendMessage(descriptor, WM_USER + 2, WPARAM(hWnd), NULL);
+		}
+		else
+		{
+			MessageBox(hWnd, __TEXT("Окно № 2 закрыто"), _TEXT("Окно № 2 не найдено"), MB_OK);
+		}
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	}
+	return 0;
+}
+
---
+++ Lab_2/Laba2_part2/WindowsProject3/laba2.cpp
@@ -0,0 +1,125 @@
+﻿// WindowsProject3.cpp : Определяет точку входа для приложения.
+//
+
+#include <Windows.h>
+#include <tchar.h>
+#include <stdio.h>
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+WCHAR WinName2[] = _TEXT("MainFrame2");
+
+
+BOOL MySystemShutdown()			//Функция выключения компютера 
+{
+	HANDLE hToken;
+	TOKEN_PRIVILEGES tkp;
+
+	// Get a token for this process. 
+
+	if (!OpenProcessToken(GetCurrentProcess(),
+		TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, &hToken))
+		return(FALSE);
+
+	// Get the LUID for the shutdown privilege. 
+
+	LookupPrivilegeValue(NULL, SE_SHUTDOWN_NAME,
+		&tkp.Privileges[0].Luid);
+
+	tkp.PrivilegeCount = 1;  // one privilege to set    
+	tkp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+	// Get the shutdown privilege for this process. 
+
+	AdjustTokenPrivileges(hToken, FALSE, &tkp, 0,
+		(PTOKEN_PRIVILEGES)NULL, 0);
+
+	if (GetLastError() != ERROR_SUCCESS)
+		return FALSE;
+
+	// Shut down the system and force all applications to close. 
+
+	if (!ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE,
+		SHTDN_REASON_MAJOR_OPERATINGSYSTEM |
+		SHTDN_REASON_MINOR_UPGRADE |
+		SHTDN_REASON_FLAG_PLANNED))
+		return FALSE;
+
+	//shutdown was successful
+	return TRUE;
+}
+
+int WINAPI wWinMain(HINSTANCE This,		 // Дескриптор текущего приложения 
+	HINSTANCE Prev, 	// В современных системах всегда 0 
+	LPTSTR cmd, 		// Командная строка 
+	int mode) 		// Режим отображения окна
+{
+	HWND hWnd;		// Дескриптор главного окна программы 
+	MSG msg; 		// Структура для хранения сообщения 
+	WNDCLASS wc; 	// Класс окна
+	// Определение класса окна 
+	wc.hInstance = This;
+	wc.lpszClassName = WinName2; 				// Имя класса окна 
+	wc.lpfnWndProc = WndProc; 					// Функция окна 
+	wc.style = CS_HREDRAW | CS_VREDRAW; 			// Стиль окна 
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); 		// Стандартная иконка 
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); 		// Стандартный курсор 
+	wc.lpszMenuName = NULL; 					// Нет меню 
+	wc.cbClsExtra = 0; 						// Нет дополнительных данных класса 
+	wc.cbWndExtra = 0; 						// Нет дополнительных данных окна 
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1); 	// Заполнение окна белым цветом 
+
+
+
+	// Регистрация класса окна
+	if (!RegisterClass(&wc)) return 0;
+
+	// Создание окна 
+	hWnd = CreateWindow(WinName2,			// Имя класса окна 
+		_TEXT("Окно № 2"), 		// Заголовок окна 
+		WS_OVERLAPPEDWINDOW, 		// Стиль окна 
+		CW_USEDEFAULT,				// x 
+		CW_USEDEFAULT, 				// y	 Размеры окна 
+		600, 				// width 
+		500, 				// Height 
+		HWND_DESKTOP, 				// Дескриптор родительского окна 
+		NULL, 						// Нет меню 
+		This, 						// Дескриптор приложения 
+		NULL); 					// Дополнительной информации нет 
+	//SetWindowLong(, GWL_STYLE, WS_CHILD);
+	ShowWindow(hWnd, mode); 				// Показать окно
+
+	// Цикл обработки сообщений 
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); 		// Функция трансляции кодов нажатой клавиши 
+		DispatchMessage(&msg); 		// Посылает сообщение функции WndProc() 
+	}
+	return 0;
+}
+
+// Оконная функция вызывается операционной системой
+// и получает сообщения из очереди для данного приложения
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+	default: 			// Обработка сообщения по умолчанию 
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	case WM_USER + 1:
+	{
+		MessageBox(hWnd, _TEXT("Получено сообщение № 1"), _TEXT("Приняли"), MB_OK);
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	case WM_USER + 2:
+	{
+		MessageBox(hWnd, _TEXT("Получено сообщение № 2"), _TEXT("Приняли"), MB_OK);
+		SendMessage(HWND(wParam), WM_USER + 3, NULL, NULL);
+		//ExitWindowsEx(EWX_SHUTDOWN | EWX_FORCE, 0);
+		MySystemShutdown();
+	}
+	}
+	return 0;
+}
